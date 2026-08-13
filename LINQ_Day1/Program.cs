9cfbcd325d9bfe9903446b97f61f0771ce9dc1d7
osama-moh-ramadan <osamamar69@gmail.com>
2026-08-13 17:56:31 +0300

    Add files via upload

---
+++ LINQ_Day1/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          List<Student> students = StudentData.GetStudents();
+            Console.WriteLine("------part 1 : Student Search-------");
+            Console.Write("Enter student name: ");
+            string name = Console.ReadLine();
+
+            var student = students.FirstOrDefault(s => s.Name == name);
+
+            if (student != null)
+            {
+                Console.WriteLine("Student Found!");
+                Console.WriteLine($"ID: {student.Id}");
+                Console.WriteLine($"Name: {student.Name}");
+                Console.WriteLine($"Department: {student.Department}");
+                Console.WriteLine($"Age: {student.Age}");
+                Console.WriteLine($"Percentage: {student.Percentage}%");
+            }
+            else
+            {
+                Console.WriteLine("Student Not Found");
+            }
+            Console.WriteLine("------part 2----------------------");
+            var studentsAbove85 = students.Where(s => s.Percentage > 85).OrderByDescending(s => s.Percentage);
+            foreach (var studentt in studentsAbove85)
+            {
+                Console.WriteLine($"{studentt.Name} - {studentt.Percentage}%");
+            }
+            Console.WriteLine("\n------ Computer Science Students ------");
+
+            var csStudents = students
+                .Where(s => s.Department == "Computer Science");
+
+            foreach (var studentt in csStudents)
+            {
+                Console.WriteLine($"{studentt.Name} - {studentt.Percentage}%");
+            }
+            Console.WriteLine("\n------ Students Between 80% and 90% ------");
+
+            var studentsBetween80And90 = students
+                .Where(s => s.Percentage >= 80 && s.Percentage <= 90);
+
+            foreach (var studentt in studentsBetween80And90)
+            {
+                Console.WriteLine($"{studentt.Name} - {studentt.Percentage}%");
+            }
+            Console.WriteLine("======================================================");
+            Console.WriteLine("-------------------------part 3:");
+            Console.Write("How many top students do you want to display? ");
+            int number = int.Parse(Console.ReadLine());
+            var topStudents = students.OrderByDescending(s => s.Percentage).Take(number);
+            Console.WriteLine($"===== Top {number} Students =====");
+            int rank = 1;
+            foreach (var studentt in topStudents)
+            {
+                Console.WriteLine($"{rank}. {studentt.Name} - {studentt.Percentage}%");
+                rank++;
+            }
+            Console.WriteLine("==================================================");
+            Console.WriteLine("--------------------------part 4---------------------");
+            Console.WriteLine("\n------ Part 4 : Student Pagination ------");
+
+            int pageSize = 3;
+
+            Console.Write("Enter page number: ");
+            int pageNumber = int.Parse(Console.ReadLine());
+
+            int skip = (pageNumber - 1) * pageSize;
+
+            var pageStudents = students.Skip(skip).Take(pageSize);
+            Console.WriteLine($"===== Page {pageNumber} =====");
+            foreach (var studentt in pageStudents)
+            {
+                Console.WriteLine($"{studentt.Id}. {studentt.Name} - {studentt.Percentage}%");
+            }
+            Console.WriteLine("===========================================");
+            Console.WriteLine("-----------------part 5-----------");
+            var departmentGroups = students.GroupBy(s => s.Department);
+            Console.WriteLine("===== Students By Department =====");
+            foreach (var group in departmentGroups)
+            {
+                double averagePercentage = group.Average(s => s.Percentage);
+
+                Console.WriteLine($"\n{group.Key}");
+                Console.WriteLine($"Students: {group.Count()}");
+                Console.WriteLine($"Average Percentage: {averagePercentage:F2}%");
+            }
+            Console.WriteLine("==========================================");
+            Console.WriteLine("----------part 6 -----------------");
+            var studentInfo = students.Select(s => new
+            {
+                s.Name,
+                s.Department,
+                s.Percentage
+            });
+            foreach (var studentt in studentInfo)
+            {
+                Console.WriteLine(
+                    $"{studentt.Name} - {studentt.Department} - {studentt.Percentage}%");
+            }
+            Console.WriteLine("=======================================");
+            Console.WriteLine("-----------------part 7-------------");
+
+        }
+    }
+}
---
+++ LINQ_Day1/data.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Day1
+{
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public int Age { get; set; }
+        public double Percentage { get; set; }
+        public List<string> Skills { get; set; }
+    }
+
+    public static class StudentData
+    {
+        public static List<Student> GetStudents()
+        {
+            return new List<Student>
+            {
+                new Student
+                {
+                    Id = 1,
+                    Name = "Ahmed Ali",
+                    Department = "Computer Science",
+                    Age = 21,
+                    Percentage = 92.5,
+                    Skills = new List<string> { "C#", "SQL", "ASP.NET Core" }
+                },
+
+                new Student
+                {
+                    Id = 2,
+                    Name = "Mona Hassan",
+                    Department = "Information Systems",
+                    Age = 22,
+                    Percentage = 87.4,
+                    Skills = new List<string> { "SQL", "Excel", "Power BI" }
+                },
+
+                new Student
+                {
+                    Id = 3,
+                    Name = "Omar Mohamed",
+                    Department = "Computer Science",
+                    Age = 20,
+                    Percentage = 95.8,
+                    Skills = new List<string> { "C#", "Java", "Problem Solving" }
+                },
+
+                new Student
+                {
+                    Id = 4,
+                    Name = "Sara Ahmed",
+                    Department = "Information Systems",
+                    Age = 21,
+                    Percentage = 78.6,
+                    Skills = new List<string> { "SQL", "HTML", "CSS" }
+                },
+
+                new Student
+                {
+                    Id = 5,
+                    Name = "Youssef Khaled",
+                    Department = "Information Technology",
+                    Age = 23,
+                    Percentage = 89.2,
+                    Skills = new List<string> { "Networking", "Linux", "Python" }
+                },
+
+                new Student
+                {
+                    Id = 6,
+                    Name = "Nour Adel",
+                    Department = "Computer Science",
+                    Age = 20,
+                    Percentage = 96.4,
+                    Skills = new List<string> { "C#", "ASP.NET Core", "Entity Framework" }
+                },
+
+                new Student
+                {
+                    Id = 7,
+                    Name = "Karim Samir",
+                    Department = "Information Technology",
+                    Age = 22,
+                    Percentage = 73.5,
+                    Skills = new List<string> { "Networking", "Windows Server", "Linux" }
+                },
+
+                new Student
+                {
+                    Id = 8,
+                    Name = "Hana Mahmoud",
+                    Department = "Information Systems",
+                    Age = 21,
+                    Percentage = 91.3,
+                    Skills = new List<string> { "SQL", "Power BI", "Excel" }
+                },
+
+                new Student
+                {
+                    Id = 9,
+                    Name = "Ali Mostafa",
+                    Department = "Computer Science",
+                    Age = 23,
+                    Percentage = 84.7,
+                    Skills = new List<string> { "C++", "Data Structures", "Algorithms" }
+                },
+
+                new Student
+                {
+                    Id = 10,
+                    Name = "Salma Tarek",
+                    Department = "Information Technology",
+                    Age = 20,
+                    Percentage = 94.1,
+                    Skills = new List<string> { "Python", "Cyber Security", "Linux" }
+                },
+
+                new Student
+                {
+                    Id = 11,
+                    Name = "Mahmoud Adel",
+                    Department = "Computer Science",
+                    Age = 22,
+                    Percentage = 76.8,
+                    Skills = new List<string> { "Java", "C++", "Problem Solving" }
+                },
+
+                new Student
+                {
+                    Id = 12,
+                    Name = "Laila Hassan",
+                    Department = "Information Systems",
+                    Age = 23,
+                    Percentage = 88.9,
+                    Skills = new List<string> { "SQL", "Business Analysis", "Excel" }
+                }
+            };
+        }
+    }
+}
+
+
+
